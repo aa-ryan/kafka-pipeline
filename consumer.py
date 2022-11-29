4c4793c0731c41deccbd7379aaf238fb5c728ffd
aa-ryan <aryansinha6363@gmail.com>
2022-11-30 01:19:31 +0530

    prod con sev

--- consumer.py
+++ consumer.py
@@ -4,11 +4,12 @@
 # get_ipython().system('pip install confluent-kafka')
 
 
-from confluent_kafka import Consumer
+from confluent_kafka import Consumer, Producer
 import random
 import json
 
 def check_severity(patient_dict):
+    description = "Risk Of "
     blood_oxygen = int(patient_dict['data']['OxygenSaturation'])
     heart_rate = int(patient_dict['data']['HeartRate'])
     blood_pressure = int(patient_dict['data']['BloodPressure'])
@@ -19,93 +20,102 @@ def check_severity(patient_dict):
     response_dict['PatientId'] = patient_dict['PatientId']
     response_dict['ward'] = patient_dict['ward']
 
-    risk_of = "Risk of: "
-    description = ""
 
     def check_temperature(temperature):
+        nonlocal description
         if 0 <= temperature < 35:
-            description += risk_of + "Severe Hypothermia"
+            description +=  " Severe Hypothermia, "
         if temperature >= 39.1:
-            description += risk_of + "Severe Hyperthermia"
+            description +=  " Severe Hyperthermia, "
         if 35 <= temperature <= 35.9:
-            description += risk_of + "Moderate Hypothermia"
+            description += " Moderate Hypothermia, "
         if 38.1 <= temperature <= 39:
-            description += risk_of + "Moderate Hyperthermia"
+            description += " Moderate Hyperthermia, "
         if 36 <= temperature <= 36.9:
-            description += risk_of + "Mild Hypothermia"
+            description += " Mild Hypothermia, "
         if 37.6 <= temperature <= 38:
-            description += risk_of + "Mild Hyperthermia"
+            description += " Mild Hyperthermia, "
 
     def check_heart_rate(heart_rate):
+        nonlocal description
         if 0 <= heart_rate < 40:
-            description += "Possible " + risk_of + "Bradyarrhythmias"
+            description += " Possible risk of Bradyarrhythmias, "
         if heart_rate > 130:
-            description += "Possible " + risk_of + "Tachyarrhythmias or Atrial Fibrillation"
+            description += " Possible risk of Tachyarrhythmias or Atrial Fibrillation, "
         if 40 <= heart_rate < 41:
-            description += "Possible" + risk_of + "Bradyarrhythmias or Sinus Bradycardia"
+            description += " Possible risk of Bradyarrhythmias or Sinus Bradycardia, "
         if 111 <= heart_rate <= 130:
-            description += "Possible" + risk_of + "Sinus Tachyarrhythmia or Tachyarrhythmias"
+            description += " Possible risk of Sinus Tachyarrhythmia or Tachyarrhythmias, "
         if 41 <= heart_rate <= 50:
-            description += "Possible" + risk_of + "Sinus Bradyarrhythmia"
-        if 91 <= heart_rate <= 100:
-            description += "Possible" + risk_of + "Sinus Tachyarrhythmia"
+            description += " Possible risk of Sinus Bradyarrhythmia, "
+        if 91 <= heart_rate <= 110:
+            description += "Possible risk of Sinus Tachyarrhythmia, "
 
     def check_respiration_rate(respiration_rate):
+        nonlocal description
         if 0 <= respiration_rate < 8:
-            description += risk_of + "Severe Bradyapnea"
+            description += " Severe Bradyapnea, "
         if respiration_rate >= 25:
-            description += risk_of + "Severe Tachypnea"
+            description += " Severe Tachypnea, "
         if 8 <= respiration_rate <= 9:
-            description += risk_of + "Moderate Bradyapnea"
+            description += " Moderate Bradyapnea, "
         if 21 <= respiration_rate <= 24:
-            description += risk_of + "Moderate Tachypnea"
+            description += " Moderate Tachypnea, "
         if 10 <= respiration_rate <= 11:
-            description += risk_of + "Mild Bradyapnea"
+            description += " Mild Bradyapnea, "
         if 19 <= respiration_rate <= 20:
-            description += risk_of + "Mild Tachypnea"
+            description += " Mild Tachypnea, "
 
     def check_blood_oxygen(blood_oxygen):
+        nonlocal description
         if 0 <= blood_oxygen <= 85:
-            description += risk_of + "Severe Hypoxia"
+            description += " Severe Hypoxia, "
         if 86 <= blood_oxygen <= 88:
-            description += risk_of + "Moderate Hypoxia"
+            description += " Moderate Hypoxia, "
         if 89 <= blood_oxygen < 92:
-            description += risk_of + "Mild Hypoxia"
+            description += " Mild Hypoxia, "
 
     def check_blood_pressure(blood_pressure):
+        nonlocal description
         if 0 <= blood_pressure <= 90:
-            description += risk_of + "Severe Hypotension"
+            description += " Severe Hypotension, "
         if blood_pressure > 220:
-            description += risk_of + "Severe Hypertension"
+            description += " Severe Hypertension, "
         if 91 <= blood_pressure <= 100:
-            description += risk_of + "Moderate Hypotension"
+            description += " Moderate Hypotension, "
         if 201 <= blood_pressure <= 220:
-            description += risk_of + "Moderate Hypertension"
+            description += " Moderate Hypertension, "
         if 101 <= blood_pressure <= 110:
-            description += risk_of + "Mild Hypotension"
+            description += " Mild Hypotension, "
         if 181 <= blood_pressure <= 200:
-            description += risk_of + "Mild Hypertension"
+            description += " Mild Hypertension, "
 
     def check_risk(blood_oxygen, heart_rate, blood_pressure, respiration_rate, temperature):
-        if (check_blood_oxygen(blood_oxygen) or check_blood_pressure(blood_pressure) or check_heart_rate(heart_rate) or check_respiration_rate(respiration_rate) or check_temperature(temperature)) == "High Risk":
+        if "Severe" in description or heart_rate < 40 or heart_rate > 130:
             return "High Risk"
-        if (check_blood_oxygen(blood_oxygen) or check_blood_pressure(blood_pressure) or check_heart_rate(heart_rate) or check_respiration_rate(respiration_rate) or check_temperature(temperature)) == "Moderate Risk":
+        if "Moderate" in description or 40 <= heart_rate <= 41 or 111 <= heart_rate <= 130:
             return "Moderate Risk"
-        if (check_blood_oxygen(blood_oxygen) or check_blood_pressure(blood_pressure) or check_heart_rate(heart_rate) or check_respiration_rate(respiration_rate) or check_temperature(temperature)) == "Low Risk":
+        if "Mild" in description or 41 <= heart_rate <= 50 or 91 <= heart_rate <= 110:
             return "Mild Risk"
-        if (check_blood_oxygen(blood_oxygen) or check_blood_pressure(blood_pressure) or check_heart_rate(heart_rate) or check_respiration_rate(respiration_rate) or check_temperature(temperature)) == "Healthy":
-            return "No Risk"
 
-    response_dict['Risk Factor'] = check_risk(blood_oxygen, heart_rate, blood_pressure, respiration_rate, temperature)
-    response_dict['Description'] = description
+    check_blood_oxygen(blood_oxygen)
+    check_heart_rate(heart_rate)
+    check_blood_pressure(blood_pressure)
+    check_respiration_rate(respiration_rate)
+    check_temperature(temperature)
 
-    return str(response_dict)
+    if check_risk(blood_oxygen, heart_rate, blood_pressure, respiration_rate, temperature) == None:
+        return None
+    else:
+        response_dict['Risk Factor'] = check_risk(blood_oxygen, heart_rate, blood_pressure, respiration_rate, temperature)
+        response_dict['Description'] = description
+        return str(response_dict)
 
 def result_producer(patient_dict):
-    from confluent_kafka import Producer
     conf = Producer(read_ccloud_config("client.properties"))
-    conf.produce("model_output", check_severity(patient_dict))
-    conf.flush()
+    if check_severity(patient_dict) != None:
+        conf.produce("model_output", check_severity(patient_dict))
+        conf.flush()
         
 def read_ccloud_config(config_file):
     conf = {}
--- producer_severe_all.py
+++ producer_severe_all.py
@@ -18,44 +18,27 @@ def read_ccloud_config(config_file):
 
 # ---- Patient ID----------------------------------------------
 lower_limit = 1000
-upper_limit = 9999
+upper_limit = 1099
 
-# patient_id = random.sample(range(lower_limit, upper_limit), 30)
-# ward = ['ward_a', 'ward_b', 'ward_c']
-# for i in range(len(patient_id)):
-#     dict_id['PatientId'] = patient_id[i]
+# ------Vitals---------------------------------------------
 
-# ------Temperature---------------------------------------------
+lower_limit_oxy = 85
+upper_limit_oxy = 94
 
-lower_limit_temp = 37.00
-upper_limit_temp = 37.50
+lower_limit_heart = 40
+upper_limit_heart = 131
 
-lower_limit_heart = 51
-upper_limit_heart = 90
+lower_limit_bp = 90
+upper_limit_bp = 220
 
-lower_limit_resp = 12
-upper_limit_resp = 18
+lower_limit_resp = 8
+upper_limit_resp = 25
 
-lower_limit_oxy = 92
-upper_limit_oxy = 100
+lower_limit_temp = 33.00
+upper_limit_temp = 40.00
 
-lower_limit_bp = 111
-upper_limit_bp = 180
 
-# for i in range(0,100):
 
-#     for j in range(len(df_id['PatientId'])):
-#         dict_details['PatientId'] = str(df_id['PatientId'][j])
-#         dict_details['ward'] = str(df_id['ward_number'][j])
-#         dict_details['Temperature'] = str(round(random.uniform(lower_limit_temp, upper_limit_temp), 2))
-#         dict_details['HeartRate'] = str(random.randint(lower_limit_heart, upper_limit_heart))
-#         dict_details['RespirationRatio'] = str(random.randint(lower_limit_resp, upper_limit_resp))
-#         dict_details['OxygenSaturation'] = str(random.randint(lower_limit_oxy, upper_limit_oxy))
-#         dict_details['BloodPressure'] = str(random.randint(lower_limit_bp, upper_limit_bp))
-
-#         df_final = df_final.append(dict_details, ignore_index=True)
-        
-        
         
 def generate_patientID():
     patient_id = random.sample(range(lower_limit, upper_limit), 30)
@@ -76,7 +59,7 @@ def generate_patient_data(patient_id, ward):
             json_dict['data']['BloodPressure'] = str(random.randint(lower_limit_bp, upper_limit_bp))
 
             producer.produce("patient_vitals", str(json_dict))
-            time.sleep(30)
+            time.sleep(1)
 
             # with (open('json_data.json', 'a')) as f:
             #     json.dump(json_dict, f, indent=2)
