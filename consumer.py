6b70e5ffda69a14c4399f426e6b47bb905f918a7
aa-ryan <aryansinha6363@gmail.com>
2022-11-29 17:14:22 +0530

    consumer and producer

--- consumer.py
+++ consumer.py
@@ -1,15 +1,96 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# get_ipython().system('pip install confluent-kafka')
 
 
-get_ipython().system('pip install confluent-kafka')
+from confluent_kafka import Consumer
+import random
+import json
 
+def check_severity(patient_dict):
+    blood_oxygen = int(patient_dict['data']['OxygenSaturation'])
+    heart_rate = int(patient_dict['data']['HeartRate'])
+    blood_pressure = int(patient_dict['data']['BloodPressure'])
+    respiration_rate = int(patient_dict['data']['RespirationRate'])
+    temperature = float(patient_dict['data']['Temperature'])
 
-# In[2]:
+    response_dict = {}
+    response_dict['PatientId'] = patient_dict['PatientId']
+    response_dict['ward'] = patient_dict['ward']
+    
+    def check_temperature(temperature):
+        if 0 <= temperature < 35 or temperature >= 39.1:
+            return "High Risk"
+        if 35 <= temperature <= 35.9 or 38.1 <= temperature <= 39:
+            return "Moderate Risk"
+        if 36 <= temperature <= 36.9 or 37.6 <= temperature <= 38:
+            return "Low Risk"
+        if 37 <= temperature <= 37.5:
+            return "Healthy"
+            
+    def check_heart_rate(heart_rate):
+        if 0 <= heart_rate < 40 or heart_rate > 130:
+            return "High Risk"
+        if 40 <= heart_rate < 41 or 111 <= heart_rate <= 130:
+            return "Moderate Risk"
+        if 41 <= heart_rate <= 50 or 91 <= heart_rate <= 100:
+            return "Low Risk"
+        if 51 <= heart_rate <= 90: 
+            return "Healthy"
+        
+    def check_respiration_rate(respiration_rate):
+        if 0 <= respiration_rate < 8 or respiration_rate >= 25:
+            return "High Risk"
+        if 8 <= respiration_rate <= 9 or 21 <= respiration_rate <= 24:
+            return "Moderate Risk"
+        if 10 <= respiration_rate <= 11 or 19 <= respiration_rate <= 20:
+            return "Low Risk"
+        if 12 <= respiration_rate <= 18:
+            return "Healthy"
+        
+        
+    def check_blood_oxygen(blood_oxygen):
+        if 0 <= blood_oxygen <= 85:
+            return "High Risk"
+        if 86 <= blood_oxygen <= 88:
+            return "Moderate Risk"
+        if 89 <= blood_oxygen < 92:
+            return "Low Risk"
+        if blood_oxygen >= 92:
+            return "Healthy"
+        
+        
+    def check_blood_pressure(blood_pressure):
+        if 0 <= blood_pressure <= 90 or blood_pressure > 220:
+            return "High Risk"
+        if 91 <= blood_pressure <= 100 or 201 <= blood_pressure <= 220:
+            return "Moderate Risk"
+        if 101 <= blood_pressure <= 110 or 181 <= blood_pressure <= 200:
+            return "Low Risk"
+        if 111 <= blood_pressure <= 180:
+            return "Healthy"
 
+    def check_risk(blood_oxygen, heart_rate, blood_pressure, respiration_rate, temperature):
+        if (check_blood_oxygen(blood_oxygen) or check_blood_pressure(blood_pressure) or check_heart_rate(heart_rate) or check_respiration_rate(respiration_rate) or check_temperature(temperature)) == "High Risk":
+            return "High Risk"
+        if (check_blood_oxygen(blood_oxygen) or check_blood_pressure(blood_pressure) or check_heart_rate(heart_rate) or check_respiration_rate(respiration_rate) or check_temperature(temperature)) == "Moderate Risk":
+            return "Moderate Risk"
+        if (check_blood_oxygen(blood_oxygen) or check_blood_pressure(blood_pressure) or check_heart_rate(heart_rate) or check_respiration_rate(respiration_rate) or check_temperature(temperature)) == "Low Risk":
+            return "Mild Risk"
+        if (check_blood_oxygen(blood_oxygen) or check_blood_pressure(blood_pressure) or check_heart_rate(heart_rate) or check_respiration_rate(respiration_rate) or check_temperature(temperature)) == "Healthy":
+            return "No Risk"
 
+    response_dict['Risk Factor'] = check_risk(blood_oxygen, heart_rate, blood_pressure, respiration_rate, temperature)
+
+    return str(response_dict)
+
+def result_producer(patient_dict):
+    from confluent_kafka import Producer
+    conf = Producer(read_ccloud_config("client.properties"))
+    conf.produce("model_output", check_severity(patient_dict))
+    conf.flush()
+        
 def read_ccloud_config(config_file):
     conf = {}
     with open(config_file) as fh:
@@ -20,51 +101,30 @@ def read_ccloud_config(config_file):
                 conf[parameter] = value.strip()
     return conf
 
+def str_to_dic(s):
+    return eval(s)
 
-# In[3]:
+if __name__ == "__main__":
+    props = read_ccloud_config("client.properties")
+    props["group.id"] = "python-group-1"
+    # print("Group ID - {}".format(pros['group.id']) )
+    props["auto.offset.reset"] = "latest"
 
+    consumer = Consumer(props)
 
-from confluent_kafka import Consumer
-import random
-
-props = read_ccloud_config("client.properties")
-props["group.id"] = "python-group-"+str(random.randint(0,8))
-props["auto.offset.reset"] = "earliest"
-
-consumer = Consumer(props)
-
-
-# In[4]:
-
-
-import json
-js = None
-consumer.subscribe(["topic_2"])
-try:
-    while True:
-        msg = consumer.poll(1.0)
-        if msg is not None and msg.error() is None:
-            print(type(msg.value().decode('utf-8')))
-            js = json.loads(msg.value().decode('utf-8'))
-        #print("key = {key:12} value = {value:12}".format(key=msg.key().decode('utf-8'), value=msg.value().decode('utf-8')))
-            #print(msg)
-            #print(type(msg))
-        else:
-            print("NONE")
-except KeyboardInterrupt:
-    pass
-finally:
-    consumer.close()
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
+    consumer.subscribe(["patient_vitals"])
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is not None and msg.error() is None:
+                # print(msg.value().decode('utf-8'))
+                print("Consumed: " + str(str_to_dic(msg.value().decode('utf-8'))))
+                result_producer(str_to_dic(msg.value().decode('utf-8')))
+            else:
+                print("NONE")
+    except KeyboardInterrupt:
+        print("BYE")
+    finally:
+        print("Closing Consumer")
+        consumer.close()
 
---
+++ producer.py
@@ -0,0 +1,100 @@
+# Produce Json from Dictionary
+import random
+import json
+from confluent_kafka import Producer
+import time, json
+
+def read_ccloud_config(config_file):
+    conf = {}
+    with open(config_file) as fh:
+        for line in fh:
+            line = line.strip()
+            if len(line) != 0 and line[0] != "#":
+                parameter, value = line.strip().split('=', 1)
+                conf[parameter] = value.strip()
+    return conf
+
+
+
+# ---- Patient ID----------------------------------------------
+lower_limit = 1000
+upper_limit = 9999
+
+# patient_id = random.sample(range(lower_limit, upper_limit), 30)
+# ward = ['ward_a', 'ward_b', 'ward_c']
+# for i in range(len(patient_id)):
+#     dict_id['PatientId'] = patient_id[i]
+
+# ------Temperature---------------------------------------------
+
+lower_limit_temp = 36.60
+upper_limit_temp = 37.50
+
+lower_limit_heart = 51
+upper_limit_heart = 100
+
+lower_limit_resp = 9
+upper_limit_resp = 14
+
+lower_limit_oxy = 96
+upper_limit_oxy = 100
+
+lower_limit_bp = 101
+upper_limit_bp = 180
+
+# for i in range(0,100):
+
+#     for j in range(len(df_id['PatientId'])):
+#         dict_details['PatientId'] = str(df_id['PatientId'][j])
+#         dict_details['ward'] = str(df_id['ward_number'][j])
+#         dict_details['Temperature'] = str(round(random.uniform(lower_limit_temp, upper_limit_temp), 2))
+#         dict_details['HeartRate'] = str(random.randint(lower_limit_heart, upper_limit_heart))
+#         dict_details['RespirationRatio'] = str(random.randint(lower_limit_resp, upper_limit_resp))
+#         dict_details['OxygenSaturation'] = str(random.randint(lower_limit_oxy, upper_limit_oxy))
+#         dict_details['BloodPressure'] = str(random.randint(lower_limit_bp, upper_limit_bp))
+
+#         df_final = df_final.append(dict_details, ignore_index=True)
+        
+        
+        
+def generate_patientID():
+    patient_id = random.sample(range(lower_limit, upper_limit), 30)
+    return patient_id
+
+def generate_patient_data(patient_id, ward):
+    producer = Producer(read_ccloud_config("client.properties"))
+    json_dict = {}
+    for i in range(0, 100):
+        for i,j in zip(patient_id, ward):
+            json_dict['PatientId'] = i
+            json_dict['ward'] = j
+            json_dict['data'] = {}
+            json_dict['data']['Temperature'] = str(round(random.uniform(lower_limit_temp, upper_limit_temp), 2))
+            json_dict['data']['HeartRate'] = str(random.randint(lower_limit_heart, upper_limit_heart))
+            json_dict['data']['RespirationRatio'] = str(random.randint(lower_limit_resp, upper_limit_resp))
+            json_dict['data']['OxygenSaturation'] = str(random.randint(lower_limit_oxy, upper_limit_oxy))
+            json_dict['data']['BloodPressure'] = str(random.randint(lower_limit_bp, upper_limit_bp))
+
+            producer.produce("topic2", str(json_dict))
+            time.sleep(5)
+
+            # with (open('json_data.json', 'a')) as f:
+            #     json.dump(json_dict, f, indent=2)
+    producer.flush()
+            
+def ward_gen():
+    ward = ['ward_a', 'ward_b', 'ward_c']
+    list_of_wards = list()
+    for i in range(0, 30):
+        list_of_wards.append(random.choice(ward))
+    return list_of_wards
+
+## Convert list of dictionaries to Json
+def convert(lst):
+    return json.dumps(lst, indent=2)
+    
+if __name__ == "__main__":
+    patient_ids, wards = generate_patientID(), ward_gen()
+    generate_patient_data(patient_ids, wards)
+        
+        
